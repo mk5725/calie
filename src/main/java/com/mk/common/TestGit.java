5446d51877a390ee8daf7d7a5f9babd6eb400f26
mk <2805836175@qq.com>
2023-09-06 10:48:56 +0800

    init TestGit.java

---
+++ src/main/java/com/mk/common/TestGit.java
@@ -0,0 +1,13 @@
+package com.mk.common;
+
+public class TestGit {
+    private int a;
+
+    public int getA() {
+        return this.a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
